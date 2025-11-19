850f251f37a743283214d01d83dfc92b260fd5c1
saichandvallapu@gmail.com <saichandvallapu@gmail.com>
2025-11-19 17:16:14 +0000

    all models

--- models/sources/Weather_bike_correlation.sql
+++ models/sources/Weather_bike_correlation.sql
@@ -2,8 +2,9 @@
 with CTE as (
     select *
     from 
-    {{ref('Trips_fact')}} 
-
+    {{ref('Trips_fact')}} t
+    left join {{ref('daily_weather')}} w
+    on t.TRIP_DATE =w.daily_weather
 )
 
 select * from CTE
